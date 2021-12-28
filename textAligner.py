2adb9f9eab455917ee414c4a8358ab744f9c6e00
Sahil Mulani <40736800+SahillMulani@users.noreply.github.com>
2021-12-28 15:42:03 +0530

    Update TextAlginer

--- textAligner.py
+++ textAligner.py
@@ -4,3 +4,18 @@ c = 'H'
 #Top Cone
 for i in range(thickness):
     print((c*i).rjust(thickness-1)+c+(c*i).ljust(thickness-1))
+#Bottom Cone
+for i in range(thickness):
+     print((c*(thickness-1)).rjust(thickness)+(c*(thickness-2)).ljust(thickness))
+#Mid Part
+for i in range(thickness-2):
+     print((c*(thickness-1)).rjust(thickness)+(c*(thickness+10)).ljust(thickness)+(c*(thickness)).rjust(thickness+10))
+         
+for i in range(thickness):
+     print((c*(thickness-1)).rjust(thickness)+(c*(thickness-2)).ljust(thickness))
+
+for i in range(thickness -3):
+     print((c*(thickness-1)).rjust(thickness)+(c*(thickness-2)).ljust(thickness))
+
+for i in range(thickness):
+     print((c*(thickness)).rjust(thickness+10))
